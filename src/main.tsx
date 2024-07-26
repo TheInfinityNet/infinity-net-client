bf456a13c44c16cbd24a50e1fa0a2cb8faaf5f43
Nguyen Minh Tuan <tnowad@gmail.com>
2024-07-27 04:23:02 +0700

    feat: add msw mock api

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./globals.css";
 import { App } from "./app";
+import { worker } from "./mocks";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+worker.start({ onUnhandledRequest: "bypass" });
--- src/routes/pages/home.page.tsx
+++ src/routes/pages/home.page.tsx
@@ -12,7 +12,6 @@ import {
   FilterIcon,
   HeartIcon,
   MessageCircleIcon,
-  MoveHorizontalIcon,
   PlusIcon,
   SettingsIcon,
   ShareIcon,
