7d377366ba84fc9fa1d7f856afef6429665876b8
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-03 03:08:22 +0700

    feat: add fake feed

--- src/components/widgets/feed.tsx
+++ src/components/widgets/feed.tsx
@@ -1,18 +1,17 @@
+import { faker } from "@faker-js/faker";
 import { PostCard } from "../post-card";
 
 export function NewsFeed() {
   return (
     <div className="grid gap-4">
-      <PostCard
-        name="Infinity Net"
-        username="@infinitynet"
-        content="Excited to announce our latest product launch! Check it out and let us know what you think."
-      />
-      <PostCard
-        name="Jane Doe"
-        username="@janedoe"
-        content="Loving the new updates to the platform! Can't wait to see what's next."
-      />
+      {Array.from({ length: 50 }).map((_, index) => (
+        <PostCard
+          key={index}
+          name={faker.person.fullName()}
+          username={`@${faker.internet.userName()}`}
+          content={faker.lorem.paragraph()}
+        />
+      ))}
     </div>
   );
 }
