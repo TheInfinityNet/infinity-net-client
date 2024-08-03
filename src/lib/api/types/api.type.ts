e5fb9bce950d0f1b179e4530b5703d5d21d4a19b
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-04 06:40:05 +0700

    feat: add metadata type for pagination

--- src/lib/api/types/api.type.ts
+++ src/lib/api/types/api.type.ts
@@ -1,3 +1,15 @@
 export type ValidationErrors<T> = {
   [K in keyof T]?: string[];
 };
+
+export type Metadata = {
+  pagination: {
+    offset: number;
+    limit: number;
+    previousOffset: number;
+    nextOffset: number;
+    currentPage: number;
+    pageCount: number;
+    totalCount: number;
+  };
+};
