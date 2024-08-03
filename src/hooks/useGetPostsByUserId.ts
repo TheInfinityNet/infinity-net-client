25814d629dfcfa78a3a073e122ad99a6db8347b4
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-04 06:41:17 +0700

    feat: create hook for get posts

---
+++ src/hooks/useGetPostsByUserId.ts
@@ -0,0 +1,11 @@
+import postsService from "@/lib/api/services/posts.service";
+import { useInfiniteQuery } from "react-query";
+
+export function useGetPostByUserId(userId: string) {
+  return useInfiniteQuery(["posts", userId], {
+    queryFn: ({ pageParam = 0 }) =>
+      postsService.getPostsByUserId(userId, { offset: pageParam, limit: 10 }),
+    getNextPageParam: (lastPage) =>
+      lastPage.data.metadata.pagination.nextOffset,
+  });
+}
