588eccab6dfdcecb850a77013274e2b11ee58014
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-05 06:51:33 +0700

    feat: add news feed infinite news feed component

--- src/components/widgets/feed.tsx
+++ src/components/widgets/feed.tsx
@@ -1,17 +1,47 @@
-import { faker } from "@faker-js/faker";
 import { PostCard } from "../post-card";
+import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useGetNewsFeed } from "@/hooks/useGetNewsFeed";
 
 export function NewsFeed() {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useGetNewsFeed();
+
+  const posts = useMemo(
+    () => data?.pages.flatMap((page) => page.data.posts) ?? [],
+    [data],
+  );
+
+  const handleLoadMore = useCallback(() => {
+    fetchNextPage();
+  }, [fetchNextPage]);
+
+  const fetchElementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+        handleLoadMore();
+      }
+    });
+
+    const currentRef = fetchElementRef.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [fetchElementRef, hasNextPage, isFetchingNextPage, handleLoadMore]);
+
   return (
-    <div className="grid gap-4">
-      {Array.from({ length: 50 }).map((_, index) => (
-        <PostCard
-          key={index}
-          name={faker.person.fullName()}
-          username={`@${faker.internet.userName()}`}
-          content={faker.lorem.paragraph()}
-        />
+    <>
+      {posts.map((post, index) => (
+        <PostCard key={index} post={post} />
       ))}
-    </div>
+      <div ref={fetchElementRef}>Loading more...</div>
+    </>
   );
 }
