6864fba1f96c6548962ae8356ce7423748daa1b1
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-04 14:02:52 +0700

    fix: make msw run before create elements

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./globals.css";
 import { App } from "./app";
-import { worker } from "./mocks";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function initializeMocks() {
+  const { worker } = await import("./mocks");
+  await worker.start({ onUnhandledRequest: "bypass", waitUntilReady: true });
+}
 
-worker.start({ onUnhandledRequest: "bypass", waitUntilReady: true });
+async function main() {
+  if (process.env.NODE_ENV !== "production") {
+    await initializeMocks();
+  }
+
+  const rootElement = document.getElementById("root");
+
+  if (!rootElement) {
+    throw new Error("Root element not found");
+  }
+
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
+
+main().catch((error) => {
+  console.error("Failed to initialize the application:", error);
+});
