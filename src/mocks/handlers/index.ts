f3ca10b189b83c343040f8e449e1b3620d62f382
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-05 06:43:04 +0700

    feat: add post handlers

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import { authHandlers } from "./auth.handlers";
+import { postsHandlers } from "./posts.handlers";
 import { profileHandlers } from "./profile.handlers";
 import { usersHandlers } from "./users.handlers";
 
-export const handlers = [...authHandlers, ...profileHandlers, ...usersHandlers];
+export const handlers = [
+  ...authHandlers,
+  ...profileHandlers,
+  ...usersHandlers,
+  ...postsHandlers,
+];
---
+++ src/mocks/handlers/posts.handlers.ts
@@ -0,0 +1,26 @@
+import { http, HttpResponse } from "msw";
+import { users } from "../data";
+import _ from "lodash";
+import { generatePost } from "../generators";
+
+export const postsHandlers = [
+  http.get("/users/:userId/posts", ({ request, params }) => {
+    const { userId } = params as { userId: string };
+    const url = new URL(request.url);
+    const offset = Number(url.searchParams.get("offset"));
+    const limit = Number(url.searchParams.get("limit"));
+
+    const user = users[userId];
+
+    return HttpResponse.json({
+      posts: _.times(10, () => generatePost({ user })),
+      metadata: {
+        pagination: {
+          offset,
+          limit,
+          nextOffset: offset + limit,
+        },
+      },
+    });
+  }),
+];
