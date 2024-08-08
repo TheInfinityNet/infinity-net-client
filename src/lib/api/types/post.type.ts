908ed675b16517b92d64a7f7a73b9d165883e57d
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-09 03:42:55 +0700

    feat: add missing field for type post

--- src/lib/api/types/post.type.ts
+++ src/lib/api/types/post.type.ts
@@ -1,8 +1,11 @@
+import { Comment } from "./comment.type";
 import { User } from "./user.type";
 
 export type Post = {
   id: string;
+  userId: string;
   user?: User;
+  comments?: Comment[];
   content: string;
   createdAt: string;
   updatedAt: string;
