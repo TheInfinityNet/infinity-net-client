01fb32adaa5cc46240e0e24b89fb54f60015f1ae
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-09 03:43:09 +0700

    feat: add notification type enum

---
+++ src/lib/api/types/notification.type.ts
@@ -0,0 +1,17 @@
+export enum NotificationType {
+  Like = "LIKE",
+  Comment = "COMMENT",
+  Reply = "REPLY",
+  Follow = "FOLLOW",
+  Message = "MESSAGE",
+  Tag = "TAG",
+  Mention = "MENTION",
+  FriendRequest = "FRIEND_REQUEST",
+  EventInvitation = "EVENT_INVITATION",
+  GroupInvitation = "GROUP_INVITATION",
+  Share = "SHARE",
+  Reaction = "REACTION",
+  Achievement = "ACHIEVEMENT",
+  Story = "STORY",
+}
+
