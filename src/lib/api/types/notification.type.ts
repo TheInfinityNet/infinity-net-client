4947cdcb626bf02393a6df66c29faa14bacedd3a
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-11 06:13:23 +0700

    feat: split type to small type

--- src/lib/api/types/notification.type.ts
+++ src/lib/api/types/notification.type.ts
@@ -1,3 +1,5 @@
+import { User } from "./user.type";
+
 export enum NotificationType {
   Like = "LIKE",
   Comment = "COMMENT",
@@ -15,90 +17,111 @@ export enum NotificationType {
   Story = "STORY",
 }
 
-type BaseNotification = {
+export type BaseNotification = {
   id: string;
   userId: string;
+  user?: User;
   isRead: boolean;
-  createdAt: Date;
+  createdAt: string;
 };
 
-type LikeNotification = BaseNotification & {
+export type LikeNotificationDetails = {
   type: NotificationType.Like;
   postId: string;
 };
 
-type CommentNotification = BaseNotification & {
+export type CommentNotificationDetails = {
   type: NotificationType.Comment;
   postId: string;
   commentId: string;
 };
 
-type ReplyNotification = BaseNotification & {
+export type ReplyNotificationDetails = {
   type: NotificationType.Reply;
   postId: string;
   commentId: string;
   replyId: string;
 };
 
-type FollowNotification = BaseNotification & {
+export type FollowNotificationDetails = {
   type: NotificationType.Follow;
   followerId: string;
 };
 
-type MessageNotification = BaseNotification & {
+export type MessageNotificationDetails = {
   type: NotificationType.Message;
   messageId: string;
   contentPreview: string;
 };
 
-type TagNotification = BaseNotification & {
+export type TagNotificationDetails = {
   type: NotificationType.Tag;
   postId: string;
   tagId: string;
 };
 
-type MentionNotification = BaseNotification & {
+export type MentionNotificationDetails = {
   type: NotificationType.Mention;
   postId: string;
   mentionId: string;
 };
 
-type FriendRequestNotification = BaseNotification & {
+export type FriendRequestNotificationDetails = {
   type: NotificationType.FriendRequest;
   requestId: string;
 };
 
-type EventInvitationNotification = BaseNotification & {
+export type EventInvitationNotificationDetails = {
   type: NotificationType.EventInvitation;
   eventId: string;
 };
 
-type GroupInvitationNotification = BaseNotification & {
+export type GroupInvitationNotificationDetails = {
   type: NotificationType.GroupInvitation;
   groupId: string;
 };
 
-type ShareNotification = BaseNotification & {
+export type ShareNotificationDetails = {
   type: NotificationType.Share;
   postId: string;
   sharedById: string;
 };
 
-type ReactionNotification = BaseNotification & {
+export type ReactionNotificationDetails = {
   type: NotificationType.Reaction;
   postId: string;
   reactionType: string;
 };
 
-type AchievementNotification = BaseNotification & {
+export type AchievementNotificationDetails = {
   type: NotificationType.Achievement;
   achievementId: string;
 };
 
-type StoryNotification = BaseNotification & {
+export type StoryNotificationDetails = {
   type: NotificationType.Story;
   storyId: string;
 };
+
+export type LikeNotification = BaseNotification & LikeNotificationDetails;
+export type CommentNotification = BaseNotification & CommentNotificationDetails;
+export type ReplyNotification = BaseNotification & ReplyNotificationDetails;
+export type FollowNotification = BaseNotification & FollowNotificationDetails;
+export type MessageNotification = BaseNotification & MessageNotificationDetails;
+export type TagNotification = BaseNotification & TagNotificationDetails;
+export type MentionNotification = BaseNotification & MentionNotificationDetails;
+export type FriendRequestNotification = BaseNotification &
+  FriendRequestNotificationDetails;
+export type EventInvitationNotification = BaseNotification &
+  EventInvitationNotificationDetails;
+export type GroupInvitationNotification = BaseNotification &
+  GroupInvitationNotificationDetails;
+export type ShareNotification = BaseNotification & ShareNotificationDetails;
+export type ReactionNotification = BaseNotification &
+  ReactionNotificationDetails;
+export type AchievementNotification = BaseNotification &
+  AchievementNotificationDetails;
+export type StoryNotification = BaseNotification & StoryNotificationDetails;
 
 export type Notification =
   | LikeNotification
