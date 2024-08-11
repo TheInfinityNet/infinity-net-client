f64e713f090f3b8f21b867160da4c9e17803ecdd
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-12 06:28:35 +0700

    feat: add utils paginate mock data

---
+++ src/mocks/utils/pagination.ts
@@ -0,0 +1,26 @@
+import { Pagination } from "@/lib/api/types/api.type";
+import _ from "lodash";
+
+export function paginate<T>(
+  totalCount: number,
+  offset: number,
+  limit: number,
+  generateFn: () => T,
+): { items: T[]; pagination: Pagination } {
+  const items = _.times(Math.min(limit, totalCount - offset), generateFn);
+  const nextOffset = offset + limit < totalCount ? offset + limit : null;
+  const previousOffset = offset - limit >= 0 ? offset - limit : null;
+
+  return {
+    items,
+    pagination: {
+      offset,
+      limit,
+      nextOffset,
+      previousOffset,
+      totalCount,
+      pageCount: Math.ceil(totalCount / limit),
+      currentPage: Math.floor(offset / limit) + 1,
+    },
+  };
+}
