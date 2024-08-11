c7755f45271aaaa4c7e1a2c989eb4f6b7b7096ae
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-12 06:28:08 +0700

    refactor: split pagination type from metadata

--- src/lib/api/types/api.type.ts
+++ src/lib/api/types/api.type.ts
@@ -2,14 +2,16 @@ export type ValidationErrors<T> = {
   [K in keyof T]?: string[];
 };
 
+export type Pagination = {
+  offset: number;
+  limit: number;
+  previousOffset: number | null;
+  nextOffset: number | null;
+  currentPage: number;
+  pageCount: number;
+  totalCount: number;
+};
+
 export type Metadata = {
-  pagination: {
-    offset: number;
-    limit: number;
-    previousOffset: number;
-    nextOffset: number;
-    currentPage: number;
-    pageCount: number;
-    totalCount: number;
-  };
+  pagination: Pagination;
 };
