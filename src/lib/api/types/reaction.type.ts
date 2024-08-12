811bbd3f4acb851bd0410db1e77e0468eec8c930
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-12 13:10:59 +0700

    feat: add reaction type for post comment and message

--- src/lib/api/types/reaction.type.ts
+++ src/lib/api/types/reaction.type.ts
@@ -17,3 +17,33 @@ export type Reaction = {
   createdAt: string;
   updatedAt: string;
 };
+
+export type PostReaction = {
+  id: string;
+  userId: string;
+  user?: User;
+  postId: string;
+  type: ReactionType;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type CommentReaction = {
+  id: string;
+  userId: string;
+  user?: User;
+  commentId: string;
+  type: ReactionType;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type MessageReaction = {
+  id: string;
+  userId: string;
+  user?: User;
+  messageId: string;
+  type: ReactionType;
+  createdAt: string;
+  updatedAt: string;
+};
