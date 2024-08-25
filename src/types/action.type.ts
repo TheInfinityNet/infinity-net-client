9d978cf9d9c29550b8c4966a829158e923ef4781
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-25 23:01:54 +0700

    feat: add post actions enum

---
+++ src/types/action.type.ts
@@ -0,0 +1,9 @@
+export enum PostActions {
+  React = 'POST_REACT',
+  Comment = 'POST_COMMENT',
+  Share = 'POST_SHARE',
+  Edit = 'POST_EDIT',
+  Delete = 'POST_DELETE',
+  Report = 'POST_REPORT',
+}
+
