aefa410471a507a45040fcaf6159dc213686a2e0
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-25 23:02:05 +0700

    feat: add user actions

--- src/types/action.type.ts
+++ src/types/action.type.ts
@@ -7,3 +7,17 @@ export enum PostActions {
   Report = 'POST_REPORT',
 }
 
+export enum UserActions {
+  SendFriendRequest = 'USER_SEND_FRIEND_REQUEST',
+  AcceptFriendRequest = 'USER_ACCEPT_FRIEND_REQUEST',
+  RejectFriendRequest = 'USER_REJECT_FRIEND_REQUEST',
+  BlockUser = 'USER_BLOCK',
+  UnblockUser = 'USER_UNBLOCK',
+  ReportUser = 'USER_REPORT',
+  FollowUser = 'USER_FOLLOW',
+  UnfollowUser = 'USER_UNFOLLOW',
+  SendMessage = 'USER_SEND_MESSAGE',
+  ViewProfile = 'USER_VIEW_PROFILE',
+  RemoveFriend = 'USER_REMOVE_FRIEND',
+}
+
