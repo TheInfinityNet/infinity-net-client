408b3d447af75bf0b80a500e1aeb96fc807af664
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-26 23:03:57 +0700

    feat: add post react action

---
+++ src/types/post-action.type.ts
@@ -0,0 +1,22 @@
+import { PostActions } from "./action.type";
+import { ReactionType } from "./reaction.type";
+
+export enum PostCurrentReactStatus {
+  Reacted = 'REACTED',
+  NotReacted = 'NOT_REACTED',
+}
+
+export type PostReactAction = {
+  [PostActions.React]: {
+    isEnable: boolean;
+    reactionCounts: {
+      [key in ReactionType]: number;
+    };
+  } & ({
+    currentStatus: PostCurrentReactStatus.Reacted;
+    currentUserReaction: ReactionType;
+  } | {
+    currentStatus: PostCurrentReactStatus.NotReacted;
+  });
+}
+
